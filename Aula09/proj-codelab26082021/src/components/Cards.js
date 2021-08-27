0a0856e4cc08458698e6ed778be4f7d1eed6e395
Flavio Wu <83521015+flaviowu@users.noreply.github.com>
2021-08-27 13:29:32 -0300

    formatação

--- Aula09/proj-codelab26082021/src/components/Cards.js
+++ Aula09/proj-codelab26082021/src/components/Cards.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import './Cards.css'
+import React from "react";
+import "./Cards.css";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <img src={props.imgUrl} className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{props.titulo}</h5>
-                <p className="card-text">{props.ano}</p>
-                {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
-            </div>
-        </div>
-    )
+  return (
+    <div className="card">
+      <img src={props.imgUrl} className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{props.titulo}</h5>
+        <p className="card-text">{props.ano}</p>
+        {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
+      </div>
+    </div>
+  );
 }
--- Aula09/proj-codelab26082021/src/components/Form.js
+++ Aula09/proj-codelab26082021/src/components/Form.js
@@ -1,20 +1,30 @@
-import React from 'react';
-import './Form.css'
+import React from "react";
+import "./Form.css";
 
 export default function Form() {
-    const handleSubmit = () => {
-        return
-    }
-    
-    return(
-        <div className='form-adm'>
-            <h3>Cadastro</h3>
-            <form name='cad-adm'>
-                <input type='text' name='titulo' placeholder='Digite o Título do Jogo' />
-                <input type='text' name='ano' placeholder='Digite o ano do Jogo' />
-                <input type='text' name='img-url' placeholder='Digite a URL da imagem' />
-                <button type='submit' onClick={handleSubmit}>Adicionar Jogo</button>
-            </form>
-        </div>
-    )
+  const handleSubmit = () => {
+    return;
+  };
+
+  return (
+    <div className="form-adm">
+      <h3>Cadastro</h3>
+      <form name="cad-adm">
+        <input
+          type="text"
+          name="titulo"
+          placeholder="Digite o Título do Jogo"
+        />
+        <input type="text" name="ano" placeholder="Digite o ano do Jogo" />
+        <input
+          type="text"
+          name="img-url"
+          placeholder="Digite a URL da imagem"
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Adicionar Jogo
+        </button>
+      </form>
+    </div>
+  );
 }
