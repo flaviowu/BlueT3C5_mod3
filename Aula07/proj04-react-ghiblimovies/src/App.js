1e493757a7f24cb0a23bb7a950cfb5fce1201810
Flavio Wu <flaviowu@gmail.com>
2021-08-27 23:24:56 -0300

    add editar e deletar filmes

--- Aula07/proj04-react-ghiblimovies/src/App.js
+++ Aula07/proj04-react-ghiblimovies/src/App.js
@@ -1,103 +1,107 @@
-import React, { useState } from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ghibliMoviesList from "./Modules/ghibliMoviesDB";
 
 export default function App() {
-  const [ nomeFilme, setNomeFilme] = useState('')
-  const [ imgUrlFilme, setImgUrlFilme] = useState('')
+  const [nomeFilme, setNomeFilme] = useState("");
+  const [imgUrlFilme, setImgUrlFilme] = useState("");
 
-  const [ ghibliMovies, setGhibliMovies ] = useState([
+  const [ghibliMovies, setGhibliMovies] = useState([...ghibliMoviesList]);
+  const [editando, setEditando] = useState(false)
+  const [idEdit, setIdEdit] = useState(null)
 
-        {
-          id: 1,
-          nome: "O Castelo Animado",
-          imgURL:
-            "https://i2.wp.com/studioghibli.com.br/wp-content/uploads/2020/04/O-castelo-animado-capa-playarte-dvd.jpg",
-        },
-
-        {
-          id: 2,
-          nome: "A Viagem de Chihiro",
-          imgURL:
-            "https://i2.wp.com/studioghibli.com.br/wp-content/uploads/2020/04/A-Viagem-de-Chihiro-DVD.jpg",
-        },
-      
-        {
-          id: 3,
-          nome: "Meu Amigo Totoro",
-          imgURL:
-            "https://i0.wp.com/studioghibli.com.br/wp-content/uploads/2020/04/meu-amigo-totoro-dvd-vers%C3%A1til.jpg",
-        },
-      
-        {
-          id: 4,
-          nome: "Princesa Monoke",
-          imgURL:
-            "https://i2.wp.com/studioghibli.com.br/wp-content/uploads/2020/04/Princesa-Mononoke-DVD-capa-vers%C3%A1til.jpg",
-        },
-      
-        {
-          id: 5,
-          nome: "O Serviço de Entregas da Kiki",
-          imgURL:
-            "https://i2.wp.com/studioghibli.com.br/wp-content/uploads/2020/04/O-Servi%C3%A7o-de-Entregas-da-Kiki-vers%C3%A1til.jpg",
-        }
-      ]
-    )
-    
-    let handleSubmit = (e) => {
-      e.preventDefault()
-      let IdFilme = ghibliMovies.length + 1
-      const novoFilme = {
-        id: IdFilme,
-        nome: nomeFilme,
-        imgURL: imgUrlFilme
-      }      
-
-      // console.log('-=-=-=-=-=-=-')
-      // setGhibliMovies(ghibliMovies.push(novoFilme))
-      console.log(novoFilme)
-      console.log(ghibliMovies)
-      setGhibliMovies([...ghibliMovies, novoFilme])
+  useEffect(() => {
+    if (idEdit !== null && editando) {
+      const movie = ghibliMovies.filter((f) => f.id === idEdit)
+      setNomeFilme(movie[0].nome)
+      setImgUrlFilme(movie[0].imgURL)
+    }
+  }, [idEdit])
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let idFilme = null
+    if (editando){
+      idFilme = idEdit
+      handleDelete(idFilme)
 
 
+    } else if(editando === false) {
+      idFilme = ghibliMovies[ghibliMovies.length - 1].id + 1;
     }
 
-    // let listarFilmes = () => {
-    //   console.log('-=-=-=-')
-    //   console.log(ghibliMovies)
-    //   console.log(nomeFilme)
-    //   console.log(imgUrlFilme)
-    //   console.log('-=-=-=-')
-    // }
+    setGhibliMovies([...ghibliMovies,
+      {
+        id: idFilme,
+        nome: nomeFilme,
+        imgURL: imgUrlFilme,
+      }
+    ])
+    
+    setEditando(false)
+    setIdEdit(null)
+    setNomeFilme("");
+    setImgUrlFilme("");
+  };
 
-    return (
-      <>
-        <div className="filmografia">
-          <h1>Filmografia Ghibli | Longa Metragens</h1>
-          <ul>
-            {ghibliMovies.map( f => (
-              <li key={f.id}>
-                <h3>{f.nome}</h3>
-                <img src={f.imgURL} alt={f.nome} />
-              </li>
-            ))}
-          </ul>
-        </div>
-        <div className="adm">
-              <h2>Administração</h2>
-              <form name="formulario" id="formulario" onSubmit={handleSubmit}>
-                <label>Título o Filme:{nomeFilme}</label>
-                <input type="text" name="nome" value={nomeFilme} onChange={(e) => {
-                  setNomeFilme(e.target.value)
-                }}></input>
-                <label>URL da Imagem:{imgUrlFilme}</label>
-                <input type="text" name="url-img" value={imgUrlFilme} onChange={(e) =>
-                  setImgUrlFilme(e.target.value)
-                }></input>
-                <button type="submit" id="cad-filme">Cadastrar</button>
-              </form>
-        </div>
-      </>
-    )
+  const handleDelete = (id) => {
+    setGhibliMovies(ghibliMovies.filter((f) => f.id !== id))
   }
+  
+  return (
+    <>
+      <div className="filmografia">
+        <h1>Filmografia Ghibli | Longa Metragens</h1>
+        <ul>
+          {ghibliMovies.map((f) => (
+            <li key={f.id}>
+              <h3>{f.nome}</h3>
+              <img src={f.imgURL} alt={f.nome} />
+              <button type="button" onClick={() => {
+                  setEditando(true)
+                  setIdEdit(f.id)
+                }}
+              >
+                Editar
+              </button>
+              <button type="button" onClick={() => handleDelete(f.id)}
+                > 
+                Excluir
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="adm">
+        
+        {/* Form */}
+        <h2>Administração</h2>
+        <form name="formulario" id="formulario" onSubmit={handleSubmit}>
+          
+          <label>Título o Filme: {nomeFilme}</label>
+          <input
+            type="text"
+            name="nome"
+            value={nomeFilme}
+            onChange={(e) => {
+              setNomeFilme(e.target.value);
+            }}
+          ></input>
 
+          <label>URL da Imagem: {imgUrlFilme}</label>
+          <input
+            type="text"
+            name="url-img"
+            value={imgUrlFilme}
+            onChange={(e) => setImgUrlFilme(e.target.value)}
+          ></input>
+
+          <button type="submit" id="cad-filme">
+            Salvar
+          </button>
+
+        </form>
+      </div>
+    </>
+  );
+}
