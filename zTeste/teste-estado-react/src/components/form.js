f96e90a797aeb5565ad2f63aeff2e90ce8bc98a5
Flavio Wu <flaviowu@gmail.com>
2021-09-03 01:46:33 -0300

    teste useEffect

--- zTeste/teste-estado-react/src/components/form.js
+++ zTeste/teste-estado-react/src/components/form.js
@@ -1,21 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
 export default function Formulario(props) {
+  const [palavra, setPalavra] = useState("");
+  const [contador, setContador] = useState(0);
+  
+  useEffect(() => {
+    setPalavra("")
+  }, [contador]);
 
-    const [ palavra, setPalavra ] = useState("")
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    setContador(contador + 1);
+    props.onSubmit(palavra);
+  }
 
-    function handleFormSubmit(e) {
-        e.preventDefault()
-        props.onSubmit(palavra)
-        setPalavra("")
-    }
-
-    return(
-        <form onSubmit={handleFormSubmit}>
-            <input type="text" placeholder="digite algo" defaultValue={palavra} onChange={ (e) => setPalavra(e.target.value)}/>
-            <button type="submit">TESTAR</button>
-        </form>
-    )
-
-
+  return (
+    <form onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        placeholder="digite algo"
+        defaultValue={palavra}
+        onChange={(e) => setPalavra(e.target.value)}
+      />
+      <button type="submit">TESTAR {contador}</button>
+    </form>
+  );
 }
